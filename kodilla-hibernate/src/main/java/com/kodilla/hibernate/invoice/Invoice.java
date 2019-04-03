a8c1916282d61ed3a71d60c41adbd6d9fe088025
KatarzynaStaniak <kstaniak11@gmail.com>
2019-04-03 09:47:30 +0200

    Invoice2

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -43,7 +43,7 @@ public class Invoice {
     @OneToMany(
             targetEntity = Item.class,
             mappedBy = "invoice",
-            cascade = CascadeType.ALL,
+            //cascade = CascadeType.ALL,
             fetch = FetchType.LAZY
     )
     public List<Item> getItems() {
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -19,28 +19,82 @@ import java.util.List;
 public class InvoiceDaoTestSuite {
     @Autowired
     private InvoiceDao invoiceDao;
+    @Autowired
+    private ProductDao productDao;
+    @Autowired
+    private ItemDao itemDao;
+
     private static final BigDecimal APPLE_PRICE = BigDecimal.valueOf(8.25);
+    private static final BigDecimal ONION_PRICE = BigDecimal.valueOf(2.51);
 
     @Test
     public void testInvoiceDaoSave() {
 
         //Given
         Product apples = new Product("apples");
+        Product onions = new Product("onions");
         Invoice invoice = new Invoice("A287");
         Item item1 = new Item(APPLE_PRICE, 3);
+        Item item2 = new Item(ONION_PRICE, 10);
+        Item item3 = new Item(APPLE_PRICE, 6);
+        Item item4 = new Item(ONION_PRICE, 4);
+
+        apples.getItems().add(item1);
+        apples.getItems().add(item3);
+        onions.getItems().add(item2);
+        onions.getItems().add(item4);
+
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+        invoice.getItems().add(item4);
+
         item1.setProduct(apples);
         item1.setInvoice(invoice);
-        apples.getItems().add(item1);
-        invoice.getItems().add(item1);
+        item3.setProduct(apples);
+        item3.setInvoice(invoice);
+        item2.setProduct(onions);
+        item2.setInvoice(invoice);
+        item4.setProduct(onions);
+        item4.setInvoice(invoice);
+
 
         //When
         invoiceDao.save(invoice);
-        int id = invoice.getId();
+        int invoiceId = invoice.getId();
+        productDao.save(apples);
+        int productAppleId = apples.getId();
+        productDao.save(onions);
+        int productOnionsId = onions.getId();
+        itemDao.save(item1);
+        int item1Id = item1.getId();
+        itemDao.save(item2);
+        int item2Id = item2.getId();
+        itemDao.save(item3);
+        int item3Id = item3.getId();
+        itemDao.save(item4);
+        int item4Id = item4.getId();
 
         //Then
-        Assert.assertNotEquals(0, id);
+        Assert.assertNotEquals(0, invoiceId);
+        Assert.assertNotEquals(0, productAppleId);
+        Assert.assertNotEquals(0, productOnionsId);
+        Assert.assertNotEquals(0, item1Id);
+        Assert.assertNotEquals(0, item2Id);
+        Assert.assertNotEquals(0, item3Id);
+        Assert.assertNotEquals(0, item4Id);
 
         //CleanUp
-        //invoiceDao.deleteById(id);
+//        try {
+//        itemDao.deleteById(item1Id);
+//        itemDao.deleteById(item2Id);
+//        itemDao.deleteById(item3Id);
+//        itemDao.deleteById(item4Id);
+//        invoiceDao.deleteById(invoiceId);
+//        productDao.deleteById(productAppleId);
+//        productDao.deleteById(productOnionsId);
+//        } catch (Exception e){
+//            System.out.println("ERRORS!");
+//        }
     }
 }
